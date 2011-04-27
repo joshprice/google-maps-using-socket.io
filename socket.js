a64e3cbf022f62cfa7c24d4793e2d6b4ef7031ff
Bayan Khalili <bayan.khalili@siteminder.com.au>
2011-04-27 20:28:23 +0800

    Serve static files

--- socket.js
+++ socket.js
@@ -1,15 +1,20 @@
-var http = require('http'),  
-    io = require('/usr/local/lib/node/.npm/socket.io/active/package/lib/socket.io'), // for npm, otherwise use require('./path/to/socket.io') 
+var http = require('http'),
+io = require('socket.io'),
+fs = require('fs');
 
-server = http.createServer(function(req, res){ 
- // your normal server code 
- res.writeHead(200, {'Content-Type': 'text/html'}); 
- res.end('<h1>Hello world</h1>'); 
+server = http.createServer(function(req, res){
+  res.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile("." + req.url, 'utf8', function (err, data) {
+      if (err) {
+        res.end();
+      }
+    res.end(data);
+  });
 });
 server.listen(3000);
-  
-// socket.io 
-var socket = io.listen(server); 
+
+// socket.io
+var socket = io.listen(server);
 
 function S4() {
   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
@@ -20,7 +25,7 @@ function guid() {
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 };
 
-socket.on('connection', function(client){ 
+socket.on('connection', function(client){
   // console.log('connection made');
 
   client.on('message', function(message){
@@ -32,7 +37,7 @@ socket.on('connection', function(client){
     var response = JSON.stringify(messageObject);
     // console.log("response", response);
     socket.broadcast(response);
-  }) 
-  // client.on('disconnect', function(){ }) 
+  })
+  // client.on('disconnect', function(){ })
 });
 
